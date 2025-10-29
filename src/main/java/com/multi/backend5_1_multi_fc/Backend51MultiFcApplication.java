5ef2cc240ed44fb050e7f1fb38fda194f5c58444
JUNHOCHOI0309 <skyho0309@naver.com>
2025-10-29 12:49:08 +0900

    build : 초기 세팅

---
+++ src/main/java/com/multi/backend5_1_multi_fc/Backend51MultiFcApplication.java
@@ -0,0 +1,13 @@
+package com.multi.backend5_1_multi_fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend51MultiFcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend51MultiFcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multi/backend5_1_multi_fc/Backend51MultiFcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.backend5_1_multi_fc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend51MultiFcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
