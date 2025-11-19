14ce8937c04ae08fdc1b5197bdc67d8d3bdc3b36
Hong Ye Rin <yerinnn526@naver.com>
2025-11-19 14:18:40 +0900

    fix : merge 후 생긴 버그 수정

--- src/main/java/com/multi/backend5_1_multi_fc/match/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-//Mybatis 매퍼 스캔 설정
-package com.multi.backend5_1_multi_fc.match.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.multi.backend5_1_multi_fc.match.mapper")
-public class MyBatisConfig { }
